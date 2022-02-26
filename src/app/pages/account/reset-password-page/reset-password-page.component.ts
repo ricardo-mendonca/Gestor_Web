83ba00c440271f945a60ee5dc13f4171ef413e19
Ricardo Mendonca <ricardo-mendonca@live.com>
2022-02-26 10:17:10 -0300

    Telas login, reset e cadastro alterado para bootstrap

--- src/app/pages/account/reset-password-page/reset-password-page.component.ts
+++ src/app/pages/account/reset-password-page/reset-password-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password-page',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password-page.component.css']
 })
 export class ResetPasswordPageComponent implements OnInit {
+  public busy = false;
+  public form!: FormGroup;
 
-  constructor() { }
+  constructor(
+    
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      ds_email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])],
+      ds_senha: ['', Validators.compose([
+        Validators.minLength(4),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  
 
 }
